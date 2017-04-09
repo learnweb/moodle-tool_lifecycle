68db77ebf34b0d511758c0fde042c1eef9448e00
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2017-04-09 10:27:30 +0200

    First structure of the module

---
+++ lang/en/local_course_deprovision.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Course Deprovision langauge strings.
+ *
+ * @package    local
+ * @subpackage course_deprovision
+ * @copyright  2017 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+
+$string['pluginname'] = 'Course Deprovision';
+$string['plugintitle'] = 'Course Deprovision';
+
+$string['subplugintype_coursedeprovisiontrigger'] = 'Trigger for starting the course deprovision';
+$string['subplugintype_coursedeprovisiontriggerplural'] = 'Triggers for starting the course deprovision';
