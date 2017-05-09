40558ccc84c24054f4dfae3a37c9239ae6b9f56e
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2017-05-09 17:04:24 +0200

    First implementation of scheduled task

--- classes/task/process_deprovision.php
+++ classes/task/process_deprovision.php
@@ -1,7 +1,39 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * Created by IntelliJ IDEA.
- * User: tore
- * Date: 5/9/17
- * Time: 12:48 PM
+ * Scheduled task for processing the deprovisioning
+ *
+ * @package local
+ * @subpackage course_deprovision
+ * @copyright  2017 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+namespace local_course_deprovision;
+
+defined('MOODLE_INTERNAL') || die;
+
+class process_deprovision extends \core\task\scheduled_task {
+
+    public function get_name() {
+        return get_string('process_deprovision', 'local_course_deprovision');
+    }
+
+    public function execute() {
+        $processor = new deprovision_processor();
+        $processor->call_trigger();
+    }
+}
