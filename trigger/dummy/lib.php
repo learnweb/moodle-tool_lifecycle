30c348450443db0af3b91e712272e4f6a7159f94
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2017-05-11 14:50:30 +0200

    Added dummy trigger for better testing possibilities

---
+++ trigger/dummy/lib.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Interface for the subplugintype trigger
+ * It has to be implemented by all subplugins.
+ *
+ * @package tool_cleanupcourses_trigger
+ * @subpackage startdatedelay
+ * @copyright  2017 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace tool_cleanupcourses\trigger;
+
+use tool_cleanupcourses\TriggerResponse;
+
+defined('MOODLE_INTERNAL') || die();
+require_once(__DIR__ . '/../lib.php');
+require_once(__DIR__ . '/../../classes/triggerresponse.php');
+
+/**
+ * Class which implements the basic methods necessary for a cleanyp courses trigger subplugin
+ * @package tool_cleanupcourses_trigger
+ */
+class dummy implements base {
+
+
+    /**
+     * Checks the course and returns a repsonse, which tells if the course should be further processed.
+     * @param $course object to be processed.
+     * @return TriggerResponse
+     */
+    public function check_course($course) {
+        return TriggerResponse::next();
+    }
+
+}
