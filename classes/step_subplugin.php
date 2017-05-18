4f99a926fff1a97baf262082e4f12563f1c879be
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2017-05-18 11:34:39 +0200

    Introduced own class for steps

---
+++ classes/step_subplugin.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_cleanupcourses;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Subplugin class
+ *
+ * @package tool_cleanupcourses
+ * @copyright  2017 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class step_subplugin extends subplugin {
+
+    /** string instancename of the step*/
+    public $instancename;
+
+    /**
+     * Creates a subplugin with name and optional id.
+     * @param string $name name of the subplugin
+     * @param int $id id of the subplugin
+     */
+    public function __construct($instancename, $name, $id = null) {
+        parent::__construct($name, $id);
+        $this->instancename = $instancename;
+    }
+
+    /**
+     * Creates a subplugin from a db record.
+     * @param $record
+     * @return step_subplugin
+     */
+    public static function from_record($record) {
+        if (!object_property_exists($record, 'name')) {
+            return null;
+        }
+        if (!object_property_exists($record, 'instancename')) {
+            return null;
+        }
+        $instance = new self($record->instancename, $record->name);
+        foreach (array_keys((array) $record) as $field) {
+            if (object_property_exists($instance, $field)) {
+                $instance->$field = $record->$field;
+            }
+        }
+
+        return $instance;
+    }
+
+}
