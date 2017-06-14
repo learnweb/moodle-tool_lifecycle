a12afe52093e243989350242e3e126727f111a0d
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2017-06-14 16:52:46 +0200

    Added first version of createbackup step

---
+++ step/createbackup/lib.php
@@ -0,0 +1,63 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Interface for the subplugintype step
+ * It has to be implemented by all subplugins.
+ *
+ * @package tool_cleanupcourses_step
+ * @subpackage createbackup
+ * @copyright  2017 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace tool_cleanupcourses\step;
+
+use tool_cleanupcourses\response\step_response;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(__DIR__ . '/../lib.php');
+
+global $CFG;
+
+require_once($CFG->dirroot . '/backup/util/includes/backup_includes.php');
+require_once($CFG->dirroot . '/backup/controller/backup_controller.class.php');
+
+class createbackup extends libbase {
+
+
+    /**
+     * Processes the course and returns a repsonse.
+     * The response tells either
+     *  - that the subplugin is finished processing.
+     *  - that the subplugin is not yet finished processing.
+     *  - that a rollback for this course is necessary.
+     * @param int $instanceid of the step instance.
+     * @param mixed $course to be processed.
+     * @return step_response
+     */
+    public function process_course($instanceid, $course) {
+        $bc = new \backup_controller(\backup::TYPE_1COURSE, $course->id, \backup::FORMAT_MOODLE,
+            \backup::INTERACTIVE_NO, \backup::MODE_AUTOMATED, get_admin()->id);
+        $bc->execute_plan();
+        $results = $bc->get_results();
+        return step_response::proceed();
+    }
+
+    public function get_subpluginname() {
+        return 'createbackup';
+    }
+}
