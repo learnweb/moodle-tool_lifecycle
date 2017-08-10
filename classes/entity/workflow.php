fe89ab1c7e3c99ab66d9c3c0def785bacbf3dc3e
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2017-08-10 16:50:48 +0200

    Added an entity for workflow definitions

---
+++ classes/entity/workflow.php
@@ -0,0 +1,82 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_cleanupcourses\entity;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Cleanup Course Workflow class
+ *
+ * @package tool_cleanupcourses
+ * @copyright  2017 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class workflow {
+
+    /** int id of the workflow*/
+    public $id;
+
+    /** title of the workflow */
+    public $title;
+
+    /** array of step instances*/
+    public $steps;
+
+    /** bool true if workflow is active*/
+    public $avtive;
+
+    /** timestamp the workflow was set active */
+    public $timeactive;
+
+    private function __construct($id, $title, $active, $timeactive) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->acvtive = $active;
+        $this->timeactive = $timeactive;
+    }
+
+    /**
+     * Creates a Workflow from a DB record.
+     * @param $record
+     * @return workflow
+     */
+    public static function from_record($record) {
+        if (!object_property_exists($record, 'id')) {
+            return null;
+        }
+        if (!object_property_exists($record, 'title')) {
+            return null;
+        }
+
+        if (object_property_exists($record, 'active') && $record->active) {
+            $active = true;
+        } else {
+            $active = false;
+        }
+
+        $timeactive = null;
+        if (object_property_exists($record, '$timeactive') && $record->timeactive) {
+            $timeactive = $record->timeactive;
+        }
+
+        $instance = new self($record->id, $record->title, $record->active, $active, $timeactive);
+
+        return $instance;
+    }
+
+}
