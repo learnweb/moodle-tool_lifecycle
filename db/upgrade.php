b93932d62b05ddf9856fcbc14e3b50492877aa2f
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2017-08-10 17:39:30 +0200

    Added db tables for workflow and worflow_definition

--- db/upgrade.php
+++ db/upgrade.php
@@ -29,5 +29,55 @@ function xmldb_tool_cleanupcourses_upgrade($oldversion) {
     global $DB;
     $dbman = $DB->get_manager();
 
+    if ($oldversion < 2017081000) {
+
+        // Create table tool_cleanupcourses_wf_steps.
+        $table = new xmldb_table('tool_cleanupcourses_wf_steps');
+
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
+        $table->add_field('workflowid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null, 'id');
+        $table->add_field('stepid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null, 'workflowid');
+        $table->add_field('sortindex', XMLDB_TYPE_INTEGER, '5', null, XMLDB_NOTNULL, null, null, 'stepid');
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch add field id.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Create table tool_cleanupcourses_workflow.
+        $table = new xmldb_table('tool_cleanupcourses_workflow');
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
+        $table->add_field('title', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null, 'id');
+        $table->add_field('active', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0', 'title');
+        $table->add_field('timeacive', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'active');
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('workflowid_fk', XMLDB_KEY_FOREIGN, array('workflowid'), 'tool_cleanupcourses_workflow', array('id'));
+        $table->add_key('stepid_fk', XMLDB_KEY_FOREIGN, array('stepid'), 'tool_cleanupcourses_step', array('id'));
+
+        // Conditionally launch add field id.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Changing precision of field followedby on table tool_cleanupcourses_step to (10).
+        $table = new xmldb_table('tool_cleanupcourses_step');
+        $field = new xmldb_field('followedby', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'subpluginname');
+
+        // Launch change of precision for field followedby.
+        $dbman->change_field_precision($table, $field);
+
+        // Changing precision of field followedby on table tool_cleanupcourses_trigger to (10).
+        $table = new xmldb_table('tool_cleanupcourses_trigger');
+        $field = new xmldb_field('followedby', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'enabled');
+
+        // Launch change of precision for field followedby.
+        $dbman->change_field_precision($table, $field);
+
+        // Cleanupcourses savepoint reached.
+        upgrade_plugin_savepoint(true, 2017081000, 'tool', 'cleanupcourses');
+    }
+
+
     return true;
 }
