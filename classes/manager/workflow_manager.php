fbaadedfc8896881355ab73e34565f429ff763c0
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2017-08-10 17:40:09 +0200

    Added simple workflow_manager

---
+++ classes/manager/workflow_manager.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Manager for Cleanup Course Workflows
+ *
+ * @package tool_cleanupcourses
+ * @copyright  2017 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace tool_cleanupcourses\manager;
+
+use tool_cleanupcourses\entity\workflow;
+
+defined('MOODLE_INTERNAL') || die();
+
+class workflow_manager {
+
+    /**
+     * Persists a workflow to the database.
+     * @param workflow $workflow
+     */
+    public static function insert_or_update(workflow &$workflow) {
+        global $DB;
+        $transaction = $DB->start_delegated_transaction();
+        if ($workflow->id) {
+            $DB->update_record('tool_cleanupcourses_workflow', $workflow);
+        } else {
+            $workflow->id = $DB->insert_record('tool_cleanupcourses_workflow', $workflow);
+        }
+        $transaction->allow_commit();
+    }
+}
