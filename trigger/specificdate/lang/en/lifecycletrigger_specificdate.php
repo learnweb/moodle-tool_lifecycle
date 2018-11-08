79efaf89515a4fd6494148dc34e016e72c80953a
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2018-11-08 15:30:42 +0100

    Added trigger for specific dates of the year.

---
+++ trigger/specificdate/lang/en/lifecycletrigger_specificdate.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for categories delay trigger
+ *
+ * @package tool_lifecycle_trigger
+ * @subpackage startdatedelay
+ * @copyright  2018 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Specific date trigger';
+
+$string['dates'] = 'Dates at which the workflow should run.';
+$string['dates_desc'] = 'Write one date per line with the format Day.Month';
+$string['timelastrun'] = 'Date when the trigger last run.';
