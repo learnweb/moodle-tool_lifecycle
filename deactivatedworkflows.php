682bcef4e3ace663843e1ba3a728d23f409719f6
Yorick Reum <webdesign@yorickreum.de>
2019-01-30 14:30:46 +0100

    disabled test, todo later

--- deactivatedworkflows.php
+++ deactivatedworkflows.php
@@ -21,6 +21,7 @@
  * @copyright  2018 Yorick Reum, JMU
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
 require_once(__DIR__ . '/../../../config.php');
 require_once(__DIR__ . '/adminlib.php');
 
