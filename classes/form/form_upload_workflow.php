e3830b8dbcf21c06fb711e342f02529056ba8762
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2019-05-13 12:15:10 +0200

    Add workflow upload form

---
+++ classes/form/form_upload_workflow.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Offers the possibility to upload a workflow in an xml format.
+ *
+ * @package    tool_lifecycle
+ * @copyright  2017 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace tool_lifecycle\form;
+
+use tool_lifecycle\entity\workflow;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . '/formslib.php');
+
+/**
+ * Provides a form to modify a workflow instance
+ */
+class form_upload_workflow extends \moodleform {
+
+
+    /**
+     * Constructor
+     * @param \moodle_url $url.
+     * @param workflow $workflow workflow entity.
+     * @throws \moodle_exception if neither step nor subpluginname are set.
+     */
+    public function __construct($url) {
+        parent::__construct($url);
+    }
+
+    /**
+     * Defines forms elements
+     */
+    public function definition() {
+        $mform = $this->_form;
+
+        $mform->addElement('filepicker', 'backupfile', get_string('file'), null,
+            array('accepted_types' => 'xml'));
+        $this->add_action_buttons('true', get_string('upload'));
+    }
+
+}
