7b3ed3f4da0bdb08202b67f0c3874294ac9abb4c
Justus Dieckmann <justusdieckmann@wwu.de>
2019-05-29 14:22:29 +0200

    Remove debugging output

--- step/createbackup/lib.php
+++ step/createbackup/lib.php
@@ -56,7 +56,6 @@ class createbackup extends libbase {
     public function process_course($processid, $instanceid, $course) {
         if (self::$numberofbackups >= settings_manager::get_settings(
                 $instanceid, SETTINGS_TYPE_STEP)['maximumbackupspercron']) {
-            mtrace('###################end backup because max');
             return step_response::waiting(); // Wait with further deletions til the next cron run.
         }
         if (backup_manager::create_course_backup($course->id)) {
@@ -82,7 +81,8 @@ class createbackup extends libbase {
     }
     public function extend_add_instance_form_definition($mform) {
         $elementname = 'maximumbackupspercron';
-        $mform->addElement('text', $elementname, get_string('createbackup_maximumbackupspercron', 'lifecyclestep_createbackup'));
+        $mform->addElement('text', $elementname,
+            get_string('createbackup_maximumbackupspercron', 'lifecyclestep_createbackup'));
         $mform->setType($elementname, PARAM_INT);
         $mform->setDefault($elementname, 10);
     }
