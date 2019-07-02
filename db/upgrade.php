ebe90937748432f43d21bf0a685c79dd5f957152
Justus Dieckmann <justusdieckmann@wwu.de>
2019-07-02 13:32:39 +0200

    Add db-table for interactionlogs

--- db/upgrade.php
+++ db/upgrade.php
@@ -327,5 +327,34 @@ function xmldb_tool_lifecycle_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2019053100, 'tool', 'lifecycle');
     }
 
+    if ($oldversion < 2019070200) {
+
+        // Define table tool_lifecycle_action_log to be created.
+        $table = new xmldb_table('tool_lifecycle_action_log');
+
+        // Adding fields to table tool_lifecycle_action_log.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('processid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('workflowid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('stepindex', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('time', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table tool_lifecycle_action_log.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('processid_fk', XMLDB_KEY_FOREIGN, ['processid'], 'tool_lifecycle_process', ['id']);
+        $table->add_key('workflowid_fk', XMLDB_KEY_FOREIGN, ['workflowid'], 'tool_lifecycle_workflow', ['id']);
+
+        // Conditionally launch create table for tool_lifecycle_action_log.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Lifecycle savepoint reached.
+        upgrade_plugin_savepoint(true, 2019070200, 'tool', 'lifecycle');
+    }
+
+
     return true;
 }
