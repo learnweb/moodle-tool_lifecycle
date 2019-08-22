61299c8003b18a64746eb564d4331297336824a0
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2019-08-22 14:49:42 +0200

    Also rollback current step

--- classes/manager/process_manager.php
+++ classes/manager/process_manager.php
@@ -163,7 +163,7 @@ class process_manager {
      */
     public static function rollback_process($process) {
         process_rollback::event_from_process($process)->trigger();
-        for ($i = $process->stepindex - 1; $i >= 1; $i--) {
+        for ($i = $process->stepindex; $i >= 1; $i--) {
             $step = step_manager::get_step_instance_by_workflow_index($process->workflowid, $i);
             $lib = lib_manager::get_step_lib($step->subpluginname);
             try {
