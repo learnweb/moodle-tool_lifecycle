423b4cfa5034ef8a83f09f2e47772bca87cf1b94
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2019-08-23 16:28:55 +0200

    Removed trailing whitespace

--- lang/en/tool_lifecycle.php
+++ lang/en/tool_lifecycle.php
@@ -30,7 +30,7 @@ $string['lifecycle:managecourses'] = 'May manage courses in tool_lifecycle';
 
 $string['general_config_header'] = "General & Subplugins";
 $string['config_delay_duration'] = 'Default duration of a course delay';
-$string['config_delay_duration_desc'] = 'This setting defines the default delay duration of a workflow 
+$string['config_delay_duration_desc'] = 'This setting defines the default delay duration of a workflow
 in case on of its processes is rolled back or finishes.
 The delay duration determines how long a course will be excepted from being processed again in either of the cases.';
 $string['active_processes_list_header'] = 'Active Processes';
