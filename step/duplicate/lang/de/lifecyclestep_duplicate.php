384679ef31628e9b048356f22bb54cc8911a94b0
Justus Dieckmann <justusdieckmann@wwu.de>
2019-08-28 20:21:25 +0200

    Translated almost everything

---
+++ step/duplicate/lang/de/lifecyclestep_duplicate.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for duplicate step
+ *
+ * @package tool_lifecycle_step
+ * @subpackage duplicate
+ * @copyright  2018 Tobias Reischmann WWU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Duplizieren-Schritt';
+
+$string['duplicate_form'] = 'Daten eingeben';
+
+$string['duplicate_course_header'] = 'Kurs duplizieren';
+$string['status_message_duplication'] = 'Duplizierter Kurs wird bald verfügbar sein.';
+$string['status_message_form'] = 'Zusätzliche Informationen benötigt';
+$string['action_new_course_data'] = '{$a} hat neuen Kursnamen eingegeben';
+
+$string['duplicate:enterdata'] = 'Daten für Kursduplikation eingeben';
