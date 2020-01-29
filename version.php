9bb1cd27674f8d80a4dbffa62758d9aec577b8b8
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2020-01-29 13:12:34 +0100

    Fix version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->maturity = MATURITY_BETA;
-$plugin->version  = 2020210100;
+$plugin->version  = 2020012100;
 $plugin->component = 'tool_lifecycle';
 $plugin->requires = 2017111300; // Require Moodle 3.4 (or above).
 $plugin->release = 'v3.8-r2';
