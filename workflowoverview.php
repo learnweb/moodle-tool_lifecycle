1a9d1b4c049f64ee9943f3b6497211ef2aca22c7
n_herr03 <nina.herrmann@uni-muenster.de>
2021-11-22 18:10:28 +0100

    cleanup and minor style changes

--- workflowoverview.php
+++ workflowoverview.php
@@ -46,19 +46,13 @@ $renderer = $PAGE->get_renderer('tool_lifecycle');
 echo $renderer->header();
 $steps = $DB->get_records('tool_lifecycle_step', array('workflowid' => $workflowid));
 $trigger = $DB->get_records('tool_lifecycle_trigger', array('workflowid' => $workflowid));
-/*
-array(1) { [3]=> object(stdClass)#6974 (5) { ["id"]=> string(1) "3" ["instancename"]=> string(8) "Category" ["subpluginname"]=> string(10) "categories" ["workflowid"]=> string(1) "3" ["sortindex"]=> string(1) "1" } }
-Category
-array(1) { [1]=> object(stdClass)#6968 (5) { ["id"]=> string(1) "1" ["instancename"]=> string(9) "A backups" ["subpluginname"]=> string(12) "createbackup" ["workflowid"]=> string(1) "3" ["sortindex"]=> string(1) "1" } }
-*/
+
 $arrayoftrigger = array();
 foreach ($trigger as $key => $value) {
-    // The Array from the DB Function uses ids as keys.
-    // Mustache cannot handle arrays which have other keys therefore a new
-    // array is build.
-    // Nice to have Icon for each subplugin.
-    // TODO right order? --> sortindex?
-    // TODO Nice to have How many courses will be caught by the trigger?
+    // The array from the DB Function uses ids as keys.
+    // Mustache cannot handle arrays which have other keys therefore a new array is build.
+    // FUTURE: Nice to have Icon for each subplugin.
+    // FUTURE: Nice to have How many courses will be caught by the trigger?
     $objectvar = (object) $trigger[$key];
     $arrayoftrigger[$objectvar->sortindex -1] = $objectvar;
     asort($arrayoftrigger);
@@ -75,6 +69,17 @@ foreach ($steps as $key => $step) {
 asort($arrayofsteps);
 
 $arrayofcourses = array();
+
+$url = new moodle_url("/admin/tool/lifecycle/workflowoverview.php", array('wf' => $workflowid));
+
+$data = [
+    'trigger' => $arrayoftrigger,
+    'steps' => $arrayofsteps,
+    'listofcourses' => $arrayofcourses,
+    'steplink' => $url
+    //'table' => $table->finish_output()
+];
+echo $OUTPUT->render_from_template('tool_lifecycle/workflowoverview', $data);
 if ($stepid) {
 
     $listofcourses = $DB->get_records_sql("SELECT p.id as processid, c.id as courseid, c.fullname as coursefullname, " .
@@ -84,10 +89,6 @@ if ($stepid) {
         "{tool_lifecycle_step} s ".
         "on p.workflowid = s.workflowid AND p.stepindex = s.sortindex " .
         "WHERE p.stepindex = :stepindex AND p.workflowid = :wfid;", array('stepindex' => $stepid, 'wfid' => $workflowid));
-    // WHERE p.stepindex = :stepindex p.workflowid = wfid
-/*    $sql = "SELECT c.shortname, c.fullname FROM {course} c INNER JOIN {tool_lifecycle_process} lp
-                     ON c.id = lp.courseid WHERE lp.stepindex = :stepindex AND lp.workflowid = :wfid";
-    $listofcourses = $DB->get_records_sql($sql, array('stepindex' => $stepid, 'wfid' => $workflowid));*/
     $listofids = array();
     foreach ($listofcourses as $key => $value) {
         $listofids = $value->courseid;
@@ -96,17 +97,6 @@ if ($stepid) {
     }
     asort($arrayofcourses);
     $table = new interaction_attention_table('tool_lifecycle_interaction', $listofids);
+    $table->out(50, false);
 }
-$url = new moodle_url("/admin/tool/lifecycle/workflowoverview.php", array('wf' => $workflowid));
-//$table->finish_output();
-$data = [
-    'trigger' => $arrayoftrigger,
-    'steps' => $arrayofsteps,
-    'listofcourses' => $arrayofcourses,
-    'steplink' => $url
-    //'table' => $table->finish_output()
-];
-// array(1) { [4]=> object(stdClass)#7131 (6) { ["id"]=> string(1) "4" ["courseid"]=> string(1) "3" ["workflowid"]=> string(1) "5" ["stepindex"]=> string(1) "1" ["waiting"]=> string(1) "1" ["timestepchanged"]=> string(10) "1634642650" } }
-echo $OUTPUT->render_from_template('tool_lifecycle/workflowoverview', $data);
-
 echo $renderer->footer();
