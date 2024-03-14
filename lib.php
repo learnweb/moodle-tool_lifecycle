33cbd0533a9dcf372ef1e8ba95b5b8b1c2b27688
Justus Dieckmann <45795270+justusdieckmann@users.noreply.github.com>
2024-03-14 12:20:25 +0100

    Show menu entry when a manual or automatic workflow is active (#199)
    * Show menu point when a manual or automatic workflow is active
    
    * Rename reset cache function
    
    * Show menu entry if there are workflows other than the default two
    
    ---------
    
    Co-authored-by: Justus Dieckmann <justusdieckmann@wwu.de>

--- lib.php
+++ lib.php
@@ -44,14 +44,16 @@ function tool_lifecycle_extend_navigation_course($navigation, $course, $context)
     }
 
     $cache = cache::make('tool_lifecycle', 'application');
-    if ($cache->has('manualworkflowexists')) {
-        $manualwfexists = $cache->get('manualworkflowexists');
+    if ($cache->has('workflowactive')) {
+        $wfexists = $cache->get('workflowactive');
     } else {
-        $manualwfexists = $DB->record_exists_select('tool_lifecycle_workflow', 'manual = 1 AND timeactive IS NOT NULL');
-        $cache->set('manualworkflowsexist', $manualwfexists);
+        $wfexists = $DB->record_exists_sql("SELECT 'yes' FROM {tool_lifecycle_workflow} wf " .
+                "JOIN {tool_lifecycle_trigger} t ON wf.id = t.workflowid " .
+                "WHERE wf.timeactive IS NOT NULL AND t.subpluginname NOT IN ('sitecourse', 'delayedcourses')");
+        $cache->set('workflowactive', $wfexists);
     }
 
-    if (!$manualwfexists) {
+    if (!$wfexists) {
         return null;
     }
 
