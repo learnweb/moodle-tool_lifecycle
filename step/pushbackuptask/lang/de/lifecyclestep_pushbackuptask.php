ab0127e4d95039733ca0a2796d412f0adf9074d9
Thomas Niedermaier <thomas.niedermaier@uni-muenster.de>
2025-06-19 05:34:23 +0200

    re-add step pushbackuptask to fix git submodule error

---
+++ step/pushbackuptask/lang/de/lifecyclestep_pushbackuptask.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for push backup task step
+ *
+ * @package    lifecyclestep_pushbackuptask
+ * @copyright  2024 Johannes Burk (HTW Berlin)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['plugindescription'] = 'Ermöglicht augenblickliche Sicherungen von Kursen durch die Verwendung von ad hoc-Tasks.';
+$string['pluginname'] = 'Ad hoc Kurssicherungs-Schritt';
+$string['privacy:metadata'] = 'Dieses Subplugin speichert keine persönlichen Daten..';
