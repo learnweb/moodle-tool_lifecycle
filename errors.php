805fb0a951d9f7324367f3e3d7c9c156203b5a44
Thomas Niedermaier <thomas.niedermaier@gmail.com>
2025-08-04 11:01:45 +0200

    fix, first try

--- errors.php
+++ errors.php
@@ -32,6 +32,7 @@ require_once(__DIR__ . '/../../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
 
 require_login();
+require_capability('moodle/site:config', context_system::instance());
 
 $syscontext = context_system::instance();
 $PAGE->set_url(new \moodle_url(urls::PROCESS_ERRORS));
