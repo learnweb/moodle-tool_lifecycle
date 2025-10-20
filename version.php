2be59ccb03e629e0bee6cbb57b37d5c5d5ec84e5
Thomas Niedermaier <thomas.niedermaier@uni-muenster.de>
2025-10-20 17:02:29 +0200

    version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->maturity = MATURITY_STABLE;
-$plugin->version  = 2025050405;
+$plugin->version  = 2025050406;
 $plugin->component = 'tool_lifecycle';
 $plugin->requires = 2022112800; // Requires Moodle 4.1+.
 $plugin->supported = [401, 405];
-$plugin->release   = 'v4.5-r6';
+$plugin->release   = 'v4.5-r7';
