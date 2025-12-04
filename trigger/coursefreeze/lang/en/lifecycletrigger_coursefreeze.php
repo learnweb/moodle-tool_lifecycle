150c9deca96988b6d64192ec64c9a69cc72c0e9d
ccaewan@ucl.ac.uk <ccaewan@ucl.ac.uk>
2025-12-04 09:20:05 +0000

    Add coursefreeze trigger subplugin with last access + creation thresholds

---
+++ trigger/coursefreeze/lang/en/lifecycletrigger_coursefreeze.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for course freeze trigger
+ *
+ * @package lifecycletrigger_coursefreeze
+ * @copyright  2025 Gifty (ccaewan)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['delay'] = 'blank.';
+$string['delay_help'] = 'blank.';
+$string['plugindescription'] = 'Triggers when Lastaccess date is older than 12 months AND Creation date is older than 24 months.';
+$string['pluginname'] = 'Course freeze trigger';
+$string['privacy:metadata'] = 'The course freeze trigger does not store personal data.';
+
