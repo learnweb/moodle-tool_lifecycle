f78ae1074ea9aeb7d8ddedfe1978cbcf2d1629b3
Thomas Niedermaier <thomas.niedermaier@uni-muenster.de>
2025-12-13 22:38:47 +0100

    change to maturity_stable

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'tool_lifecycle';
-$plugin->maturity = MATURITY_RC;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->version  = 2025102302;
 $plugin->requires = 2022112800; // Requires Moodle 4.1+.
 $plugin->supported = [405, 500];
