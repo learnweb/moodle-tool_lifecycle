b3b6fad84e6484901dbf57bcaab10b82d52da236
ccaewan@ucl.ac.uk <ccaewan@ucl.ac.uk>
2025-12-17 13:10:30 +0000

    Adjusted wording in version.php

--- step/uclcontextfreeze/version.php
+++ step/uclcontextfreeze/version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'lifecyclestep_uclcontextfreeze';
-$plugin->version   = 2025121700;
+$plugin->version   = 2025102300;
 $plugin->requires  = 2022112800; // Requires Moodle 4.1+.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '0.1';
