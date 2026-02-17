54eaad2dd6dc61092bac460b61b5b768c58c7de6
Thomas Niedermaier <thomas.niedermaier@uni-muenster.de>
2026-02-17 15:09:41 +0100

    opencast step: update version dependencies 4.5

--- step/opencast/version.php
+++ step/opencast/version.php
@@ -31,7 +31,7 @@ $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-        'tool_lifecycle' => 2026012001,
+        'tool_lifecycle' => 2025050409,
         'tool_opencast' => 2024111105,
         'block_opencast' => 2025072506,
 ];
