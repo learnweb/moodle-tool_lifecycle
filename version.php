1b3c42fce6643f988e217583c708236216bad56b
Gifty Wanzola <g.wanzola@ucl.ac.uk>
2026-04-13 16:03:01 +0100

    Update version to 2026012005 and maintain release

--- version.php
+++ version.php
@@ -13,7 +13,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * Version details.
  *
@@ -26,7 +25,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'tool_lifecycle';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->version = 2026012004;
+$plugin->version = 2026012005;
 $plugin->requires = 2024100700; // Requires Moodle 4.5+.
 $plugin->supported = [405, 501];
 $plugin->release   = 'v5.1-r5';
