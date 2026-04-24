9b72479ff78ab6b9d4e3baf57f605492b425e812
Thomas Niedermaier <thomas.niedermaier@uni-muenster.de>
2026-04-25 01:39:44 +0200

    fix lifecycle_is_plugin_installed-call in restore_workflow

--- classes/local/backup/restore_lifecycle_workflow.php
+++ classes/local/backup/restore_lifecycle_workflow.php
@@ -32,6 +32,8 @@ use tool_lifecycle\local\manager\trigger_manager;
 use tool_lifecycle\local\manager\settings_manager;
 use tool_lifecycle\settings_type;
 
+require_once(__DIR__ . '/../../../locallib.php');
+
 /**
  * Class to restore a workflow.
  * @package    tool_lifecycle
