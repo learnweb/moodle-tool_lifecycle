243bd660f3daeda84befc9c6daab875487e2ecb2
André Menrath <99024746+Menrath@users.noreply.github.com>
2026-05-14 04:10:47 +0200

    fix: add handling of potential null returns (#303)
    * fix: add handling of potential null returns
    
    #294
    
    The following functions might return null:
    tool_lifecycle\local\manager\step_manager::get_step_instance_by_workflow_index()
    tool_lifecycle\local\manager\lib_manager::get_step_lib()
    
    * phpcs
    
    * write logic differently

--- classes/local/manager/process_manager.php
+++ classes/local/manager/process_manager.php
@@ -350,8 +350,12 @@ class process_manager {
     public static function abort_process($process) {
         if (($process->stepindex ?? false) && ($process->workflowid ?? false)) {
             $step = step_manager::get_step_instance_by_workflow_index($process->workflowid, $process->stepindex);
-            $steplib = lib_manager::get_step_lib($step->subpluginname);
-            $steplib->abort_course($process);
+            if (null !== $step) {
+                $steplib = lib_manager::get_step_lib($step->subpluginname);
+                if (null !== $steplib) {
+                    $steplib->abort_course($process);
+                }
+            }
         }
         self::remove_process($process);
     }
