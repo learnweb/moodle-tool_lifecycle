3ea27922e555f3869d8c4a423118b51cf13195ec
Thomas Niedermaier <thomas.niedermaier@gmail.com>
2026-07-21 04:26:56 +0200

    opencast step: add table lifecyclestep_opencast_process_status

--- db/upgrade.php
+++ db/upgrade.php
@@ -715,5 +715,32 @@ function xmldb_tool_lifecycle_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2026012003, 'tool', 'lifecycle');
     }
 
+    if ($oldversion < 2026060101) {
+
+        // Define table lifecyclestep_opencast_process_status to be created.
+        $table = new xmldb_table('lifecyclestep_opencast_process_status');
+
+        // Adding fields to table lifecyclestep_opencast_process_status.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('processid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('stepid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('status', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('decision', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table lifecyclestep_opencast_process_status.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+
+        // Conditionally launch create table for lifecyclestep_opencast_process_status.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Lifecycle savepoint reached.
+        upgrade_plugin_savepoint(true, 2026060101, 'tool', 'lifecycle');
+
+    }
+
     return true;
 }
