1462b91a6a0380344af4d2f191c2506a29a3e8f7
Thomas Niedermaier <thomas.niedermaier@uni-muenster.de>
2026-08-13 08:09:00 +0200

    opencast step: add message.php

---
+++ step/opencast/db/messages.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Level Up XP.
+//
+// Level Up XP is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Level Up XP is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Level Up XP.  If not, see <https://www.gnu.org/licenses/>.
+//
+// https://levelup.plus
+
+/**
+ * Messages
+ *
+ * @package    lifecyclestep_opencast
+ * @copyright  2026 Thomas Niedermaier University Münster
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$messageproviders = [
+    'report' => [
+        'capability'  => 'moodle/site:config',
+    ],
+    'error' => [
+        'capability'  => 'moodle/site:config',
+    ],
+];
